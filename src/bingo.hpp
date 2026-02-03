e583cc0a77120e7f00060054c144eefae4521341
ego <ego@localhost>
2026-02-03 21:59:56 +0100

    Button implementiert

--- src/bingo.hpp
+++ src/bingo.hpp
@@ -5,5 +5,22 @@
 
 #include <Arduino.h>
 
+
+// Typen
+typedef enum processState_t { DONE, INIT, RUNNING } processState_t;
+
+// Klassen
+class Bingo_Button {
+	private:
+		uint8_t pin;
+	public:
+		Bingo_Button(uint8_t p) : pin(p) {
+				pinMode(pin, INPUT_PULLUP);
+		}
+		void update(uint8_t &state) {
+				if(digitalRead(pin) == LOW) state++;
+		}
+};
+
 #endif	// BINGO_HPP
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include "bingo.hpp"
+// Konstanten
+const uint8_t PIN_BUTTON     = GPIO_NUM_19;                     // Input-Button-Pin - LOW aktiv
+
+// Globals
+uint8_t buttonFlag = 0;
+
+// Instanzen
+Bingo_Button button(PIN_BUTTON);  // Button-Objekt
 
 void setup() {
+	Serial.begin(115200);
+	delay(1000);  // Warte auf Serial-Monitor
+	Serial.println("\nBingo gestartet.");
 }
 
 void loop() {
+	button.update(buttonFlag);
+
+	// Test Start
+	if (buttonFlag) {
+		Serial.println("Button gedrückt");
+	}
+	// Test Ende
+	
+	if(buttonFlag) {
+		buttonFlag = 0;
+		delay(500);  // Entprellzeit
+	}
 }

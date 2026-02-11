9975dd2ddd4d8fc76d9ae65a025168b93cfc70ec
ego <ego@localhost>
2026-02-11 12:19:57 +0100

    Totaler Umbau und anschliessende Umbenennung

--- src/bingo.hpp
+++ src/bingo.hpp
@@ -16,77 +16,66 @@ typedef struct {
 	uint8_t output;
 	uint8_t buttonFlag;
 	uint8_t bupressed;
-	processState_t prState;
-
-//	uint16_t jobHoldTime; 
 } Config_t;	// Konfigurationsstruktur
 
-//typedef gpios_t* pGpios_t;
-
-// Klassen
 class Button {
 	private:
-		uint8_t pin;
+		const uint8_t pin;
 	public:
-		Button(const uint8_t p) : pin(p) { pinMode(pin, INPUT_PULLUP); }
-		void update(uint8_t &state) { if(digitalRead(pin) == LOW) state++; }
+		Button(Config_t &rg) : pin(rg.btnPin) { pinMode(pin, INPUT_PULLUP); }
+		void update(Config_t &rg) { if(digitalRead(pin) == LOW) rg.bupressed++; }
 };
 
 class Manager {
 	private:
 		uint8_t max;
 	public:
-		Manager(const uint8_t m) : max(m) {}
-		void update(uint8_t &id, uint8_t state);
+		Manager(Config_t &rg) : max(rg.prMax) {}
+		void update(Config_t &rg);
 };
 
-class Job_Basis {
+class Show {
 	private:
-		processState_t state;
-		enum waitTime { ID_CHANGE = 200	};
+		const gpios_t leds; 
+		uint8_t dat;
+	public:
+		Show(Config_t &rg):leds(rg.leds), dat(0) {}
+		void init();
+		void update(const Config_t &rg);
+};
+
+class JobBasis {
 	public:
 		const uint8_t ID;		
 		uint8_t &out;
-		uint16_t holdTime;
-		Job_Basis(const uint8_t prID, uint8_t &rOut, const uint16_t prHoldTime) : ID(prID), out(rOut), holdTime(prHoldTime), state(NONE) {}
+		processState_t state;
+		JobBasis(const uint8_t prID, uint8_t &rOut) : ID(prID), out(rOut), state(NONE) {}
 		virtual void doInit() const = 0;
 		virtual void doRun() const = 0;
 	  void update(const uint8_t prID);
 };
 
-class  Job_Next : public Job_Basis {
+class  JobNext : public JobBasis {
 	public:
-		Job_Next(const uint8_t prID, uint8_t &rOut, const uint16_t prHoldTime) : Job_Basis(prID, rOut, prHoldTime) {}
-		virtual void doInit() const override { out = 0xFF; }
+		uint16_t holdTime;
+		uint8_t initVal;
+		JobNext(const uint8_t prID, Config_t &rg, const uint16_t prHoldTime, const uint8_t prInitVal) :
+			JobBasis(prID, rg.output), holdTime(prHoldTime), initVal(prInitVal) {}
+		virtual void doInit() const override { out = initVal; }
 		virtual void doRun() const override { out = 0x00; }
 };
 
 
-class Show {
-	private:
-		const gpios_t &leds; 
-		uint8_t dat;
-	public:
-		Show(const gpios_t &rLeds):leds(rLeds), dat(0) {}
-		void init();
-		void update(const uint8_t out);
-};
-
 // Implementationen
 
-void Manager::update(uint8_t &id, uint8_t state) {
- 	if(state > 0) {
-		uint8_t temp = id + 1;
-		id = temp % (max + 1);
+void Manager::update(Config_t &rg) {
+ 	if(rg.bupressed > 0) {
+		uint8_t temp = rg.id + 1;
+		rg.id = temp % (max + 1);
+		rg.bupressed = 0;
+		rg.buttonFlag = 1;
 	};
- }
-
-void Job_Basis::update(const uint8_t prID) {
-	if(prID != ID) { state = NONE; return; }
-	if(prID == ID && state == NONE) { state = SLEEP; out = 0; delay(ID_CHANGE); return; }
-	if(state == SLEEP) { state = INIT; doInit(); delay(ID_CHANGE); return; }
-	if(state == INIT) { state = RUNNING; doRun(); delay(ID_CHANGE); return; }
-	if(state == RUNNING) { doRun(); return; }		
+	if(rg.id == 0) rg.output = 0;
 }
 
 void Show::init() {
@@ -96,12 +85,20 @@ void Show::init() {
 	}
 }
 
-void Show::update(const uint8_t out) {
-	if(dat != out) return;
-	dat = out;
+void Show::update(const Config_t &rg) {
+	if(dat != rg.output) return;
+	dat = rg.output;
 	for(uint8_t i = 0; i < 8; i++) {
 		digitalWrite(leds.pin[i], (dat >> i) & 0x01);
 	}
+}
+
+void JobBasis::update(const uint8_t prID) {
+	if(prID != ID) { state = NONE; return; }
+	if(prID == ID && state == NONE) { state = SLEEP; out = 0; return; }
+	if(state == SLEEP) { state = INIT; doInit(); return; }
+	if(state == INIT) { state = RUNNING; doRun(); return; }
+	if(state == RUNNING) { doRun(); return; }		
 }
 
 #endif	// BINGO_HPP

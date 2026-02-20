c84633618c536e9837465c44fff9973792cafd78
ego <ego@localhost>
2026-02-20 12:36:07 +0100

    Version 2 fertig. Testversion noch enthalten. Button + Show wurden angepasst.

--- src/bingo.hpp
+++ src/bingo.hpp
@@ -5,25 +5,23 @@
 
 #include <Arduino.h>
 
-// Typen
-typedef struct { uint8_t pin[8]; } ledPins_t;																				// nimmt LED-GPIOs auf
+// Globaler Konfigurationstyp
 typedef struct {
 	const uint8_t prMax;
-	const ledPins_t leds;
+	const uint8_t leds[8];
 	const uint8_t btnPin;
 	const uint16_t holdTime[3];
 	const uint8_t pattern[3];
 	uint8_t id;
 	uint8_t output;
-} Config_t;	// Konfigurationsstruktur
+} Config_t;	
 
 class Button {
 	private:
-		const uint8_t pin;
-		const uint8_t prMax;		
+		Config_t &rg;
 	public:
-		Button(Config_t &rg) : pin(rg.btnPin), prMax(rg.prMax) { pinMode(pin, INPUT_PULLUP); }
-		void update(Config_t &rg);
+		Button(Config_t &rg) : rg(rg) { pinMode(rg.btnPin, INPUT_PULLUP); }
+		void update();
 };
 
 class Handler {
@@ -37,55 +35,46 @@ class Handler {
 
 class Show {
 	private:
-		const ledPins_t leds; 
+		Config_t &rg;
 		uint8_t dat;
 	public:
-		Show(Config_t &rg):leds(rg.leds), dat(0) {}
-		void init();
-		void update(const Config_t &rg);
+		Show(Config_t &rg) : rg(rg), dat(0) {
+			for(uint8_t i = 0; i < 8; i++) {
+				pinMode(rg.leds[i], OUTPUT);
+				digitalWrite(rg.leds[i], LOW);
+			}
+		}
+		void update();
 };
 
-// Implementationen
+// Implementierung der Methoden
 
-void Button::update(Config_t &rg) {
+void Button::update() {
 	static uint32_t lastTime = 0;
 	uint32_t now = millis();
-	if((now > lastTime) && (digitalRead(pin) == LOW)) {
+	if((now > lastTime) && (digitalRead(rg.btnPin) == LOW)) {
 		lastTime = now + 200;  // Entprellzeit
 		rg.id++;
-		rg.id %= prMax;
-		rg.output = 0;
+		rg.id %= rg.prMax;
 	}
 }	
 
 void Handler::update() {
-	if(rg.id == 0) {
-		rg.output = 0;
-		return;
-	}
 	uint32_t now = millis();
 	if(now < nextTime) return;
 	nextTime = now + rg.holdTime[rg.id];
 	switch (rg.id) {
-	case 1: if(rg.output == 0) rg.output = rg.pattern[1]; else rg.output <<= 1;
-		break;
-	case 2: if(rg.output == 0) rg.output = rg.pattern[2]; else rg.output >>= 1;
-		break;
+		case 0: rg.output = 0; break;		
+		case 1: if(rg.output == 0) rg.output = rg.pattern[1]; else rg.output <<= 1; break;
+		case 2: if(rg.output == 0) rg.output = rg.pattern[2]; else rg.output >>= 1; break;
 	}
 }
 
-void Show::init() {
-	for(uint8_t i = 0; i < 8; i++) {
-		pinMode(leds.pin[i], OUTPUT);
-		digitalWrite(leds.pin[i], LOW);
-	}
-}
-
-void Show::update(const Config_t &rg) {
+void Show::update() {
 	if(dat != rg.output) return;
 	dat = rg.output;
 	for(uint8_t i = 0; i < 8; i++) {
-		digitalWrite(leds.pin[i], (dat >> i) & 0x01);
+		digitalWrite(rg.leds[i], (dat >> i) & 0x01);
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,6 @@ Config_t co = {
 	.output = 0
  };
 
-uint8_t f0(const uint16_t hold);
-uint8_t f1(const uint16_t hold);
-uint8_t f2(const uint16_t hold);
-uint8_t (*funcArr[])(const uint16_t) = { f0, f1, f2 };  					// Array von Zeigern auf die Prozess-Funktionen
-uint16_t holdTime[] = { 0, 1000, 500 };  													// Haltezeiten für die Prozess-Funktionen
-
 Button button(co);  																							// Button-Objekt mit Referenz auf Konfigurationsstruktur
 Handler handler(co);  																						// Handler-Objekt mit Referenz auf Konfigurationsstruktur
 Show show(co);                                          					// Show-Objekt mit Referenz auf Konfigurationsstruktur
@@ -33,46 +27,13 @@ void test(Config_t &rg) {
 void setup() {
 	Serial.begin(115200);
 	delay(1000);  																									// Warte auf Serial-Monitor
-	show.init();  																									// Initialisiere die Show (LEDs)
 	Serial.println("\nApp gestartet.");
 }
 
 void loop() {
-	button.update(co);
+	button.update();
 	handler.update();
-
-//	co.output = funcArr[co.id](holdTime[co.id]);										// Rufe die aktuelle Prozess-Funktion auf
-	show.update(co);
-
+	show.update();
 	test(co);
-
-	delay(100);  
-}
-
-// Implementierungen der Prozess-Funktionen
-
-uint8_t f0(const uint16_t hold) { return 0b00000000; }
-
-uint8_t f1(const uint16_t hold) {
-	const uint8_t pattern = 0b00000001;  														// Initial-Muster
-	static uint32_t nextTime = 0;
-	static uint8_t data = 0;
-	uint32_t now = millis();
-	if(now > nextTime) {
-		nextTime = now + hold;
-		if(data == 0) data = pattern; else data <<= 1;
-	}
-	return data; 
-}
-
-uint8_t f2(const uint16_t hold) {
-	const uint8_t pattern = 0b10000000;  														// Initial-Muster
-	static uint32_t nextTime = 0;
-	static uint8_t data = 0;
-	uint32_t now = millis();
-	if(now > nextTime) {
-		nextTime = now + hold;
-		if(data == 0) data = pattern; else data >>= 1;
-	}
-	return data; 
+	delay(10);  																										// Kurze Pause, um CPU-Last zu reduzieren
 }

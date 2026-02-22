cf370e993cae8457cb834e4a2f4273c07b67cf4d
ego <ego@localhost>
2026-02-22 10:20:53 +0100

    Version 4 vorbereitet. Tastendruck Lang+Kurz add. Lang -> Handler nicht impl.

--- src/bingo.hpp
+++ src/bingo.hpp
@@ -5,6 +5,8 @@
 
 #include <Arduino.h>
 
+typedef enum { NOCLICK = 0, SHORTCLICK, LONGCLICK } ClickType_t;
+
 // Globaler Konfigurationstyp
 typedef struct {
 	const uint8_t prMax;
@@ -14,13 +16,15 @@ typedef struct {
 	const uint8_t pattern[3];
 	uint8_t id;
 	uint8_t output;
+	ClickType_t click;
 } Config_t;	
 
 class Button {
 	private:
 		Config_t &rg;
+		const uint16_t longPressTime;
 	public:
-		Button(Config_t &rg) : rg(rg) { pinMode(rg.btnPin, INPUT_PULLUP); }
+		Button(Config_t &rg, uint16_t lt);
 		void update();
 };
 
@@ -38,28 +42,41 @@ class Show {
 		Config_t &rg;
 		uint8_t dat;
 	public:
-		Show(Config_t &rg) : rg(rg), dat(0) {
-			for(uint8_t i = 0; i < 8; i++) {
-				pinMode(rg.leds[i], OUTPUT);
-				digitalWrite(rg.leds[i], LOW);
-			}
-		}
+		Show(Config_t &rg);
 		void update();
 };
 
 // Implementierung der Methoden
 
+Button::Button(Config_t &rg, uint16_t lt) : rg(rg), longPressTime(lt) {
+	pinMode(rg.btnPin, INPUT_PULLUP);
+}
+
 void Button::update() {
-	static uint32_t lastTime = 0;
-	uint32_t now = millis();
-	if((now > lastTime) && (digitalRead(rg.btnPin) == LOW)) {
-		lastTime = now + 200;  // Entprellzeit
-		rg.id++;
-		rg.id %= rg.prMax;
+	static uint32_t nextTime = 0;
+	static bool busy = false;
+	if(busy) {
+		if(millis() < nextTime) return;
+		uint8_t c1 = digitalRead(rg.btnPin);
+		rg.click = (c1 == LOW) ? LONGCLICK : SHORTCLICK;
+		while(digitalRead(rg.btnPin) == LOW);  																		// Warte auf Loslassen
+		busy = false;
+	} else {
+		uint8_t c0 = digitalRead(rg.btnPin);
+		if(c0 == LOW) {
+			busy = true;
+			nextTime = millis() + longPressTime;  																	// Warte auf möglichen Longclick
+			delay(10);  																														// Entprellzeit
+		}
+		rg.click = NOCLICK;
 	}
 }	
 
 void Handler::update() {
+	if(rg.click == SHORTCLICK) {		
+		rg.id++;
+		rg.id %= rg.prMax;
+	}
 	uint32_t now = millis();
 	if(now < nextTime) return;
 	nextTime = now + rg.holdTime[rg.id];
@@ -67,6 +84,14 @@ void Handler::update() {
 		case 0: rg.output = 0; break;		
 		case 1: if(rg.output == 0) rg.output = rg.pattern[1]; else rg.output <<= 1; break;
 		case 2: if(rg.output == 0) rg.output = rg.pattern[2]; else rg.output >>= 1; break;
+	}
+	rg.click = NOCLICK;
+}
+
+Show::Show(Config_t &rg) : rg(rg), dat(0) {
+	for(uint8_t i = 0; i < 8; i++) {
+		pinMode(rg.leds[i], OUTPUT);
+		digitalWrite(rg.leds[i], LOW);
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include "bingo.hpp"
 
+#define LONG_PRESS_TIME 1000  																		// Zeit in ms, die für einen Longclick benötigt wird
+
 // Konfiguration
 Config_t co = {
 	.prMax = 3,
 	.leds = { GPIO_NUM_18, GPIO_NUM_3, GPIO_NUM_4, GPIO_NUM_5, GPIO_NUM_13, GPIO_NUM_14, GPIO_NUM_16, GPIO_NUM_17 },
  	.btnPin = GPIO_NUM_19,
-	.holdTime = { 0, 1000, 500 },
+	.holdTime = { 20, 1000, 500 },
 	.pattern = { 0, 0b00000001, 0b10000000 },
 	.id = 0,
-	.output = 0
+	.output = 0,
+	.click = NOCLICK
  };
 
-Button button(co);  																							// Button-Objekt mit Referenz auf Konfigurationsstruktur
+Button button(co, LONG_PRESS_TIME);  															// Button-Objekt mit Referenz auf Konfigurationsstruktur
 Handler handler(co);  																						// Handler-Objekt mit Referenz auf Konfigurationsstruktur
 Show show(co);                                          					// Show-Objekt mit Referenz auf Konfigurationsstruktur
 
@@ -23,7 +26,6 @@ void test(Config_t &rg) {
 	}
 }
 
-// Main
 void setup() {
 	Serial.begin(115200);
 	delay(1000);  																									// Warte auf Serial-Monitor
